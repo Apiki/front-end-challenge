c1ebf41a0596ae505ebc9404f220c538e32a1ea7
Amstrong Martins <amstrong.martins@gmail.com>
2019-12-18 17:03:18 -0300

    Populando os elementos com os dados da requisição

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
 const url_endp = 'https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518';
+
 const wrap = document.querySelector('.last-posts');
 
 async function connectAPI(url) {
     await fetch(url)
-        .then(res => {
-            res.json();
-        })
+        .then(
+            async (res) => {
+                return await res.json();
+            }
+        )
 
-        .then(data => {
-            console.log(data);
-        })
+        .then(
+            data => {
+                data.map((item) => {
+                    const section = document.createElement('section');
+                    const image = document.createElement('img');
+                    const title = document.createElement('h3');
+                    let text = document.createTextNode(item.title.rendered);
 
-        .catch(err => {
-            console.log('erro: ', err.statusText);
-        })
+                    image.setAttribute('src', item._embedded['wp:featuredmedia'][0].source_url);
+                    title.appendChild(text);
+
+                    section.appendChild(image);
+                    section.appendChild(title);
+                    wrap.appendChild(section);
+                });
+            }
+        )
 }
 
-const section = document.createElement('section');
-const image = document.createElement('img');
-const title = document.createElement('h3');
-const text = document.createTextNode('Olá mundo');
+function generateCards(data) {
 
-title.appendChild(text);
-image.setAttribute('src', 'url');
+}
 
-section.appendChild(image);
-section.appendChild(title);
+//const section = document.createElement('section');
+//const image = document.createElement('img');
+//const title = document.createElement('h3');
+//const text = document.createTextNode('Olá mundo');
 
-wrap.appendChild(section);
+//title.appendChild(text);
+//image.setAttribute('src', 'url');
+
+//section.appendChild(image);
+//section.appendChild(title);
+
+//wrap.appendChild(section);
 
 connectAPI(url_endp);
