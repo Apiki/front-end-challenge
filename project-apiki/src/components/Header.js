2af2c0f97903773002a6fd703bafbafbcdbd0157
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-09-12 20:26:14 -0300

    Header finalizado

--- project-apiki/src/components/Header.js
+++ project-apiki/src/components/Header.js
@@ -2,15 +2,25 @@ import React, { useState, useEffect } from 'react';
 import '../layout/css/header.css';
 import apikiIcon from '../layout/images/marca-apiki.png';
 
-const menuHamburgerBtn = (setNavDisplay, navDisplay, setNavStyle) => {
+const menuHamburgerBtn = (setNavDisplay, navDisplay, setNavStyle, transitionMenuHambuger, setTransitionMenuHambuger) => {
   return (
     <button
       className="menu-hamburger-btn"
       onClick={() => {
         if (!navDisplay) {
+          setTransitionMenuHambuger({
+            lineOne: 'translate(0px, 10px) rotate(45deg)',
+            lineTwo: '0',
+            lineThree: 'translate(0px, -10px) rotate(-45deg)'
+          });
           setNavStyle({ animationName: 'displayNav', left: '-100%' });
           setNavDisplay(!navDisplay);
         } else {
+          setTransitionMenuHambuger({
+            lineOne: 'translate(0px, 0px) rotate(0deg)',
+            lineTwo: '1',
+            lineThree: 'translate(0px, 0px) rotate(0deg)'
+          });
           setNavStyle({ animationName: 'coverNav', left: '0%' });
           setTimeout(() => {
             setNavDisplay(!navDisplay);
@@ -18,9 +28,18 @@ const menuHamburgerBtn = (setNavDisplay, navDisplay, setNavStyle) => {
         }
       }}
     >
-      <div className="menu-line-1"></div>
-      <div className="menu-line-2"></div>
-      <div className="menu-line-3"></div>
+      <div
+        className="menu-line-1"
+        style={{ transform: transitionMenuHambuger.lineOne }}
+      ></div>
+      <div
+        className="menu-line-2"
+        style={{ opacity: transitionMenuHambuger.lineTwo }}
+      ></div>
+      <div
+        className="menu-line-3"
+        style={{ transform: transitionMenuHambuger.lineThree }}
+      ></div>
     </button>
   );
 }
@@ -44,6 +63,11 @@ const NavBar = (navStyle) => {
 const Header = () => {
   const [navDisplay, setNavDisplay] = useState(true);
   const [navStyle, setNavStyle] = useState({});
+  const [transitionMenuHambuger, setTransitionMenuHambuger] = useState({
+    lineOne: '',
+    lineTwo: '',
+    lineThree: '',
+  });
 
   useEffect(() => {
     if (window.innerWidth < 1201) {
@@ -61,7 +85,7 @@ const Header = () => {
   return (
     <header>
       <div className="header-container">
-        {menuHamburgerBtn(setNavDisplay, navDisplay, setNavStyle)}
+        {menuHamburgerBtn(setNavDisplay, navDisplay, setNavStyle, transitionMenuHambuger, setTransitionMenuHambuger)}
         <img className="apiki-icon" src={apikiIcon} alt="ícone Apiki" />
         {navDisplay && NavBar(navStyle)}
         <button className="btn-default font-ibm-plex btn-register">CADASTRAR E-MAIL</button>
