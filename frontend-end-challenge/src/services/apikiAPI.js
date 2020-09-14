d6cb39237654b23c94dceb5d4ecb4ffd23ec4b69
Hfreitas <hfreitas86@gmail.com>
2020-09-13 21:30:38 -0300

    Criadas funções de request a api

---
+++ frontend-end-challenge/src/services/apikiAPI.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+/* criando nova instância do axios para facilitar
+criação de função de request a API */
+const api = axios.create({
+  baseURL: 'https://blog.apiki.com/wp-json/wp/v2/',
+  headers: { Accept: 'application/json' },
+});
+
+// Função para obtenção de post por página
+const fetchBlogPosts = async (page = 1) => {
+  try {
+    const response = await api.get(
+      encodeURI(`/posts?_embed&categories=518&page=${page}`),
+    );
+    const { data, headers } = response;
+
+    return { data, headers };
+  } catch (error) {
+    return error;
+  }
+};
+
+// Função para obtenção de post especifíco
+const fetchBlogPost = async (slug) => {
+  try {
+    const response = await api.get(
+      `https://blog.apiki.com/wp-json/wp/v2/posts?_embed&slug=${slug}`,
+    );
+    const { data } = response;
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { fetchBlogPosts, fetchBlogPost };
