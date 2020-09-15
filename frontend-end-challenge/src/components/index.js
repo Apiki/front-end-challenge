b91decdd75e3a665972e91eb40996e1cc008d717
Hfreitas <hfreitas86@gmail.com>
2020-09-14 23:08:29 -0300

    iniciando componente header

--- frontend-end-challenge/src/components/index.js
+++ frontend-end-challenge/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Button } from './Button/Button';
 export { default as PostGrid } from './PostGrid/PostGrid';
+export { default as Loading } from './Loading/Loading';
+export { default as Header } from './Header/Header';
