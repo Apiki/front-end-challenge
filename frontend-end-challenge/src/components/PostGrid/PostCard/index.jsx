ddaf4f1a2116ddc7cdf7b8d36dc7322feb1ee33d
Hfreitas <hfreitas86@gmail.com>
2020-09-15 11:34:11 -0300

    estilização de PostCard ok

--- frontend-end-challenge/src/components/PostGrid/PostCard/index.jsx
+++ frontend-end-challenge/src/components/PostGrid/PostCard/index.jsx
@@ -2,29 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import NoImage from '../../../assets/No-image-available.png';
+import './styles.css';
 
 const PostCard = ({ slug, title, _embedded }) => {
   return (
-    <div>
-      <div>
-        <img
-          src={
-            _embedded['wp:featuredmedia']
-              ? _embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail
-                  .source_url
-              : NoImage
-          }
-          alt={
-            _embedded['wp:featuredmedia']
-              ? _embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail
-                  .file
-              : 'Imagem indisponível'
-          }
-        />
+    <div className="card-base card">
+      <div className="card-image">
+        <figure className="image is-square">
+          <img
+            src={
+              _embedded['wp:featuredmedia']
+                ? _embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail
+                    .source_url
+                : NoImage
+            }
+            alt={
+              _embedded['wp:featuredmedia']
+                ? _embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail
+                    .file
+                : 'Imagem indisponível'
+            }
+          />
+        </figure>
       </div>
-      <div>
-        <h3>{title}</h3>
-        <Link to={`/${slug}`}>Ler mais</Link>
+      <div className="card-content">
+        <h4 className="title is-4">{title}</h4>
+        <Link to={`/${slug}`}>
+          <p className="content">Ler mais</p>
+        </Link>
       </div>
     </div>
   );
