7c7ee72d95b0c2e05e00c62302bba0a20da794e8
RobertoAsgon <robertoasgon@gmail.com>
2020-09-16 19:40:46 -0400

    Adicionando componente Header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './stylesheet.css';
+import LogoApiki from '../../assets/logoapiki.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return ( <h1>Header</h1> );
+  return (
+    <header className="fade-in-top">
+      <container>
+        <img src={LogoApiki} alt="Apiki Logo"></img>
+        <div>
+          <Link id="Nav" to="/">Home</Link>
+          <Link id="Nav" to="#">Cadastrar Email</Link>
+        </div>
+      </container>
+    </header>
+  );
 }
 
 export default Header;
