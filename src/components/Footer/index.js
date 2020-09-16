2a8079e608e05042f5713d04d2933a7e9457c506
RobertoAsgon <robertoasgon@gmail.com>
2020-09-16 19:42:02 -0400

    Adicionando componente Footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import './stylesheet.css';
+import LogoApiki from '../../assets/logoapiki.png';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import RssFeedIcon from '@material-ui/icons/RssFeed';
 
 const Footer = () => {
-  return ( <h1>Footer</h1> );
+  return (
+    <footer>
+        <img className="logo-footer" src={LogoApiki} alt="LogoApiki"></img>
+
+        <div className="adress-footer">
+          <h3>Governador Valadares - MG</h3>
+          <p>Praça Doutor João Paulo Pinheiro</p>
+          <p>100 - Centro. CEP: 35010-330</p>
+          <p>+55 33 3278-1002</p>
+        </div>
+
+        <div className="adress-footer">
+          <h3>São Paulo - SP</h3>
+          <p>Rua Oscar Freire, 2379</p>
+          <p>Pinheiros</p>
+          <p>CEP: 05409-012</p>
+        </div>
+
+        <div className="icons-footer">
+          <FacebookIcon id="color"/>
+          <LinkedInIcon id="color"/>
+          <InstagramIcon id="color"/>
+          <TwitterIcon id="color"/>
+          <YouTubeIcon id="color"/>
+          <GitHubIcon id="color"/>
+          <RssFeedIcon id="color"/>
+        </div>
+  
+    </footer>
+  );
 }
 
 export default Footer;
