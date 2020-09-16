3835238057df0175a0db431651f7c72789f7fd47
RobertoAsgon <robertoasgon@gmail.com>
2020-09-16 19:35:57 -0400

    Adicionando page Home,Loader e componente PostCard.

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './stylesheet.css';
+import Undefined from '../../assets/undefined.png';
+import DefaultGravatar from '../../assets/defaultGravatar.png';
 
-const PostCard = () => {
-  return ( <h1>PostCard</h1> );
+const PostCard = ({post}) => {
+  return (
+    <Link to={`/${post.slug}`} className="card scale-up-center">
+
+      {console.log('PostCard', post)}
+
+      <img 
+        src={post['_embedded']["wp:featuredmedia"] ? post['_embedded']["wp:featuredmedia"][0].source_url : Undefined} 
+        alt={post['_embedded']["wp:featuredmedia"] ? post['_embedded']["wp:featuredmedia"][0].alt_text : 'Undefined'} 
+      />
+      
+      <h3>{post['title']["rendered"]} </h3>
+
+      <p>{post['excerpt']["rendered"].slice(3, -5)}</p> 
+
+      <span>
+        <img
+          src={ post._embedded.author[0].avatar_urls ? post._embedded.author[0].avatar_urls[24] : DefaultGravatar }
+          alt={ post._embedded.author[0].name || 'Default Gravatar'}
+        />
+        | { post['_embedded']["author"][0].name } - {new Date(post.date).toLocaleDateString() }.
+      </span>
+
+    </Link>
+  );
 }
 
 export default PostCard;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchLatestPosts, fetchMorePosts } from '../../services/api';
+import PostCard from '../../components/PostCard';
+import ReactLoading from 'react-loading';
 import './stylesheet.css';
 
 const Home = () => {
-  return ( <h1>Home</h1> );
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [disableButton, setDisableButton] = useState(false);
+  const [page, setPage] = useState(2);
+
+  useEffect(() => {
+    fetchLatestPosts()
+    .then((json) => {
+      setPosts(json);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const renderMorePosts = async () => {
+    setIsLoading(true)
+    fetchMorePosts(page)
+    .then((res) => {
+      const totalPages = res.headers.get('X-WP-TotalPages')
+      console.log('Máximo de paginas disponíveis:', totalPages)
+      console.log('Pagina atual:', page) 
+      if (+page >= totalPages) setDisableButton(true);
+      return res.json();
+    })
+    .then((res) => {
+      setPosts([...posts ,...res]);
+      setPage(page + 1);
+      setIsLoading(false);
+    });
+  }
+
+  return (
+    <main className="home-page">
+
+      {console.log('Posts', posts)}
+      {console.log('IsLoading', isLoading)}
+      {console.log('EnableButton', disableButton)}
+
+      {/* <PageSpeed /> */}
+      
+      <h1>Últimas postagens</h1>
+
+      <section className="latest-posts">
+
+        {posts.map((post) => <PostCard key={post.id} post={post}/> )}
+
+      </section>
+
+      {!isLoading && <button class="load-more-btn" disabled={disableButton} onClick={renderMorePosts}>Carregar mais</button>}
+
+      {isLoading && <div> <ReactLoading type={"spin"} color={"#FF8C00"} height={150} width={150} /></div> }
+
+    </main>
+  );
 }
 
 export default Home;
