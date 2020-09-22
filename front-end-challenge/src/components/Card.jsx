82677bba742fb2047f27612433facdfdd5e073bd
Lucas Costa Ventura <lucascv333@gmail.com>
2020-09-22 11:32:04 -0300

    arrumando componente card para dar refresh quando for clicado

--- front-end-challenge/src/components/Card.jsx
+++ front-end-challenge/src/components/Card.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Context from '../Context/Context';
 import PropTypes from 'prop-types';
+import { goToPost } from '../services/goToPost';
 
 export default function Card({ post }) {
   const { semImagem } = useContext(Context);
 
   return (
     <div className="card-pc">
-      <Link className="card link" to={`/${post.slug}`}>
+      <Link className="card link" onClick={() => goToPost(post.slug)}>
         <img
           src={(post._embedded['wp:featuredmedia']) ? post._embedded['wp:featuredmedia'][0].source_url : semImagem}
           alt="Imagem destacada"
@@ -17,7 +18,6 @@ export default function Card({ post }) {
           <h3 className="link">{post.title.rendered}</h3>
           <Link
             className="link font-grey"
-            to={`/${post.slug}`}
           >
             {post.slug}
           </Link>
--- front-end-challenge/src/components/User.jsx
+++ front-end-challenge/src/components/User.jsx
@@ -8,7 +8,7 @@ export default function User() {
   return (
     <div className="card-sm">
       <img src={internaData[0]._embedded.author[0].avatar_urls[`${url}`]} alt="Avatar do Autor" />
-      <div>
+      <div className="text-center margin-left">
         <h4>{internaData[0]._embedded.author[0].name}</h4>
         <p>{internaData[0]._embedded.author[0].description}</p>
       </div>
