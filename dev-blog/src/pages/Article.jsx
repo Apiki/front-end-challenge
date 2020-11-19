4a172244e5945c4530dd0ebe3476609946600706
Renan Oliveira <oliveira.renan.rg@gmail.com>
2020-11-18 22:34:24 -0300

    Recuperando artigo da API e renderizando

--- dev-blog/src/pages/Article.jsx
+++ dev-blog/src/pages/Article.jsx
@@ -2,13 +2,50 @@ import { Component } from 'react';
 import Header from '../components/Header';
 
 class Article extends Component {
+  constructor(props) {
+    super(props);
+    this.fetchArticle = this.fetchArticle.bind(this);
+    this.renderArticle = this.renderArticle.bind(this);
+    this.state = {
+      isLoading: true,
+      article: '',
+    }
+  }
+
+  renderArticle() {
+    const { article } = this.state;
+    return (
+      <div>
+        <img src={article[0]._embedded.['wp:featuredmedia'][0].media_details.sizes.large.source_url} alt=""/>
+        <h1>{article[0].title.rendered}</h1>
+        <div dangerouslySetInnerHTML={{ __html: article[0].content.rendered }} />
+      </div>
+    );
+  }
+
+  fetchArticle(id) {
+    this.setState({ isLoading: true }, async () => {
+      const responseReturned = await fetch(`https://blog.apiki.com/wp-json/wp/v2/posts?_embed&slug=${id}`);
+      const responseObj = await responseReturned.json();
+      this.setState({
+        isLoading: false,
+        article: responseObj,
+      })
+    })
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id
+    this.fetchArticle(id);
+  }
+
   render() {
-    const path = window.location.pathname.split('/');
-    console.log(path[path.length -1]);
+    const { isLoading } = this.state;
+    const loadingElement = <div>Loading...</div>
     return (
       <div>
         <Header />
-        <h1>Artigo</h1> 
+        {isLoading ? loadingElement : this.renderArticle()} 
       </div>
     );
   }
