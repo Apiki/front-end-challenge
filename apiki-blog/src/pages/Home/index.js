043c1526496f374a8c1343ee02a6bcd7ad8fb041
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-01-31 07:07:41 -0300

    CSS Home desktop done

--- apiki-blog/src/pages/Home/index.js
+++ apiki-blog/src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllBlogs } from '../../store/ducks/blog';
 import { useHistory } from 'react-router-dom';
-import BlogCard from '../../components/BlogCard/BlogCard';
+import BlogCard from '../../components/blogCard/BlogCard';
 import Header from '../../components/Header/Header';
 import './index.css';
 
@@ -53,107 +53,3 @@ const Home = () => {
 };
 
 export default Home;
-import BlogService from '../../services/blogService';
-
-/** Actions Types */
-
-export const Types = {
-  FIRST_PAGE_FETCHED: 'FIRST_PAGE_FETCHED',
-  POST_FETCHED: 'POST_FETCHED',
-  FETCHING: 'FETCHING',
-  ERROR: 'ERROR',
-  ADDITIONAL_PAGE_FETCHED: 'ADDITIONAL_PAGE_FETCHED',
-};
-
-/** Reducers */
-
-const initialState = {
-  isFetching: false,
-  blogs: [],
-  totalPages: 0,
-  post: [],
-  errors: [],
-};
-
-const blogReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case Types.SALES_FETCHING:
-      return { ...state, isFetching: payload.status };
-    case Types.FIRST_PAGE_FETCHED:
-      return {
-        ...state,
-        blogs: [...payload.data],
-        totalPages: payload.headers['x-wp-totalpages'],
-      };
-    case Types.ADDITIONAL_PAGE_FETCHED:
-      return { ...state, blogs: [...state.blogs, ...payload] };
-    case Types.POST_FETCHED:
-      return { ...state, post: [...payload] };
-    case Types.ERROR:
-      return {
-        ...state,
-        errors: [...state.errors, payload],
-      };
-    default:
-      return state;
-  }
-};
-
-/** Actions */
-
-export const salesFetching = (status) => ({
-  type: Types.FETCHING,
-  payload: {
-    status,
-  },
-});
-
-export const firstPageFetched = (payload) => ({
-  type: Types.FIRST_PAGE_FETCHED,
-  payload,
-});
-
-export const additionalPageFetched = (payload) => ({
-  type: Types.ADDITIONAL_PAGE_FETCHED,
-  payload,
-});
-
-export const postFetched = (payload) => ({
-  type: Types.POST_FETCHED,
-  payload,
-});
-
-export const hasErrored = (error = []) => ({
-  type: Types.ERROR,
-  payload: error,
-});
-
-/** Actions Creators */
-
-export const getAllBlogs = (page) => (dispatch) => {
-  dispatch(salesFetching(true));
-
-  BlogService.getBlogsByPage(page)
-    .then((response) => {
-      if (page === 1) {
-        // first page will overwrite the blog object in reducer
-        dispatch(firstPageFetched(response));
-        dispatch(salesFetching(false));
-      } else {
-        // others page will add news pages to the blog object in reducer
-        dispatch(additionalPageFetched(response.data));
-        dispatch(salesFetching(false));
-      }
-    })
-    .catch((error) => dispatch(hasErrored(error)));
-};
-
-export const getOnePost = (slug) => (dispatch) => {
-  dispatch(salesFetching(true));
-  BlogService.getPostBySlug(slug).then((response) => {
-    dispatch(postFetched(response.data));
-    dispatch(salesFetching(false));
-  });
-};
-
-export default blogReducer;
