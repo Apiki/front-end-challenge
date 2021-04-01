e8676a0fdd0f8d8262b216f8aae022dc510b4b95
Anderson Santana <anderslash@gmail.com>
2021-03-31 21:44:27 -0300

    Corrigindo identacao para facilitar leitura

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,17 +9,27 @@ class Card extends Component {
     return (
       <div className="col-lg-6">
         <div className="card border-dark text-light">
-            {_embedded['wp:featuredmedia'] ? <img className="card-img" alt ="" src={_embedded['wp:featuredmedia'][0].media_details.sizes['jnews-360x180'].source_url} /> :  <img className="card-img" alt ="" src={placeholder} />}
-            {/* <img className="card-img" alt ="" src={_embedded['wp:featuredmedia'][0].media_details.sizes['jnews-360x180'].source_url} /> */}
-            <div className="card-img-overlay">
-              <h2 className="card-title h4">{title.rendered}</h2>
-              <div className="col-sm-8">
-                <p className="card-text small" dangerouslySetInnerHTML={{ __html: excerpt.rendered }} />
-              </div>
-              <Link to={`/article/${slug}`} >
-                <input class="btn btn-dark" type="button" value="Ler este artigo"></input>
-              </Link>
+          {_embedded['wp:featuredmedia'] ? (
+            <img className="card-img"
+              alt =""
+              src={_embedded['wp:featuredmedia'][0].media_details.sizes['jnews-360x180'].source_url}
+            />
+          ) : (
+            <img
+              className="card-img"
+              alt =""
+              src={placeholder}
+            />
+          )}
+          <div className="card-img-overlay">
+            <h2 className="card-title h4">{title.rendered}</h2>
+            <div className="col-sm-8">
+              <p className="card-text" dangerouslySetInnerHTML={{ __html: excerpt.rendered }} />
             </div>
+            <Link to={`/article/${slug}`} >
+              <input class="btn btn-dark" type="button" value="Ler este artigo"></input>
+            </Link>
+          </div>
         </div>
       </div>
     )
