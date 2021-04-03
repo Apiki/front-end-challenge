32d497d2b92889289c97a4d77599d32d83050fdf
Anderson Santana <anderslash@gmail.com>
2021-04-03 12:35:53 -0300

    Corrigindo className na Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,9 @@ import '../style/sidebar.css';
 class Sidebar extends Component {
   render() {
     return (
-      <div class="sidebar single">
+      <div className="sidebar single">
         <div className="section">
-        <div class="fb-page" data-href="https://www.facebook.com/ApikiWordPress" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ApikiWordPress" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ApikiWordPress">Apiki WordPress</a></blockquote></div>
+        <div className="fb-page" data-href="https://www.facebook.com/ApikiWordPress" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/ApikiWordPress" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/ApikiWordPress">Apiki WordPress</a></blockquote></div>
         </div>
         <div className="section popular">
           <h2 className="section-title">Popular</h2>
