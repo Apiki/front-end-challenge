66fe6f930af7be2d02e8ff2b741b007e06eb33cf
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-05 12:39:24 -0300

    fixed card text overflow. setted loadmore button to hide and show

--- src/services/buttonHandler.js
+++ src/services/buttonHandler.js
@@ -1,7 +1,11 @@
 import getMore from "./getMore";
 
+const list = document.getElementsByClassName('main-page-load-more');
+
 const buttonHandler = async () => {
+  list[0].style.display = 'none';
   const next = await getMore();
+  list[0].style.display = 'flex';
   return next;
 }
 
