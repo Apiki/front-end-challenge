0d38bc52df1dc29080353292aa432008ee21006d
Muller Esposito <mulleresposito@hotmail.com>
2021-04-14 11:42:57 -0300

    feat(Routes): creates the routes;

--- apiki/src/index.js
+++ apiki/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 
+import './index.css';
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
