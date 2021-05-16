517b6b1114a881fc1f7dadbd6d3a4a24d8620309
Ighor Alves <ighoralc@gmail.com>
2021-05-16 05:17:17 -0300

    voltei o nome normal

--- src/home.js
+++ src/home.js
@@ -1,4 +1,4 @@
-import './style.scss';
+import './style.css';
 
 function Home() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './index.css';
 import Home from './home';
 
 ReactDOM.render(
