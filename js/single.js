ac3331c3919bd72c1ee04380d2b8532cdcbd3844
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2021-08-12 18:18:44 -0300

    adding custom  mt and md to single

--- js/single.js
+++ js/single.js
@@ -1,8 +1,9 @@
 const fetchPost = () => {
   const title = document.getElementById('post__title');
+  const metaTitle = document.querySelector('title');
+  const metaDescription = document.querySelector('meta[name="description"]');
   const excerpt = document.querySelector('.post__excerpt');
   const author = document.querySelector('.post__author');
-  const authorImage = document.querySelector('.post__author--image')
   const image = document.getElementById('post__image');
   const content = document.getElementById('post__content');
 
@@ -12,9 +13,10 @@ const fetchPost = () => {
     .then((response) => response.json())
     .then((data) => {
       title.innerText = data[0].title.rendered;
+      metaTitle.innerText = data[0].title.rendered;
+      metaDescription.setAttribute('content', data[0].excerpt.rendered);
       excerpt.innerHTML = data[0].excerpt.rendered;
       author.innerText = "by " + data[0]._embedded.author[0].name;
-      // authorImage.setAttribute('src', data[0]._embedded.author[0].avatar_urls.24);
       image.setAttribute('src', data[0]._embedded['wp:featuredmedia'][0].source_url);
       content.innerHTML = data[0].content.rendered;
     });
