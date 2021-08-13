40ae2a91171f42ebb0a281ccddf9577fbef4c7b2
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2021-08-12 23:14:32 -0300

    indentation fixes

--- js/single.js
+++ js/single.js
@@ -1,23 +1,26 @@
 const fetchPost = () => {
-  const title = document.getElementById('post__title');
-  const metaTitle = document.querySelector('title');
+  const title = document.getElementById("post__title");
+  const metaTitle = document.querySelector("title");
   const metaDescription = document.querySelector('meta[name="description"]');
-  const excerpt = document.querySelector('.post__excerpt');
-  const author = document.querySelector('.post__author');
-  const image = document.getElementById('post__image');
-  const content = document.getElementById('post__content');
+  const excerpt = document.querySelector(".post__excerpt");
+  const author = document.querySelector(".post__author");
+  const image = document.getElementById("post__image");
+  const content = document.getElementById("post__content");
 
-  let slug = new URL(document.location).searchParams.get('slug');
+  let slug = new URL(document.location).searchParams.get("slug");
   let url = `https://blog.apiki.com/wp-json/wp/v2/posts?_embed&slug=${slug}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       title.innerText = data[0].title.rendered;
       metaTitle.innerText = data[0].title.rendered;
-      metaDescription.setAttribute('content', data[0].excerpt.rendered);
+      metaDescription.setAttribute("content", data[0].excerpt.rendered);
       excerpt.innerHTML = data[0].excerpt.rendered;
       author.innerText = "by " + data[0]._embedded.author[0].name;
-      image.setAttribute('src', data[0]._embedded['wp:featuredmedia'][0].source_url);
+      image.setAttribute(
+        "src",
+        data[0]._embedded["wp:featuredmedia"][0].source_url
+      );
       content.innerHTML = data[0].content.rendered;
     });
 };
