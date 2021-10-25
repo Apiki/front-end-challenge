c2eb97300843fb188313bf4c58a4652606e5a039
Pedro Toselli <phtoselli@gmail.com>
2021-10-25 10:47:01 -0300

    Refactor: refactoring 'basicFetch' function

--- frontend-challenge/src/api/basicFetch.js
+++ frontend-challenge/src/api/basicFetch.js
@@ -1,9 +1,8 @@
-const API_URL_BASE = 'https://blog.apiki.com/';
-const API_END_POINT = 'wp-json/wp/v2/'
-
 const basicFetch = async (url, endpoint) => {
   const requisition = await fetch(`${url}${endpoint}`);
   const json = await requisition.json();
 
   return json;
 }
+
+export default basicFetch;
