977b48165ea3a375216ed2f98d2d7576e34d51d6
Pedro Toselli <phtoselli@gmail.com>
2021-10-25 12:28:07 -0300

    Refactor: refactoring structure of Header and adding basic style

--- frontend-challenge/src/components/Header.js
+++ frontend-challenge/src/components/Header.js
@@ -5,12 +5,18 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header--container header--nav">
-        <div className="header--nav-logo">Logo</div>
-        <nav className="header--nav-links">Links</nav>
+        <div className="header--nav-logo">
+          <img id='logo' src='https://apiki.com/wp-content/uploads/2020/11/logo-apiki-site-01.png' alt="Apiki logo"/>
+        </div>
+        <nav className="header--nav-links">
+          <a href="https://www.instagram.com/phtoselli/">Intagram</a>
+          <a href="https://www.linkedin.com/in/pedrotoselli/">Linkedin</a>
+          <a href="https://github.com/Pedro-Toselli">Github</a>
+        </nav>
       </div>
       <div className="header--container header--search-bar">
         <form>
-          <input type="text" placeholder="Pesquisar..." />
+          <input type="text" placeholder="Procurar postagem..." />
           <button type="submit">Search</button>
         </form>
       </div>
