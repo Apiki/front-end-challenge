0869169f57e0e1e80d5c7a8da2c48caa2c9e9449
Pedro Toselli <phtoselli@gmail.com>
2021-10-25 01:18:34 -0300

    Build: add React Router dependencies

--- frontend-challenge/src/index.js
+++ frontend-challenge/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
