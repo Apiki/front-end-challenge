1932a5c78e00a729ef405f9ee4f4aab37968fad6
Pedro Toselli <phtoselli@gmail.com>
2021-10-25 13:15:10 -0300

    Style: adding new style for Post

--- frontend-challenge/src/components/Post.js
+++ frontend-challenge/src/components/Post.js
@@ -3,10 +3,11 @@ import '../style/Post.css';
 
 export default function Post({ img, title, content}) {
   return (
-    <article className="article">
-      <img src={img} alt={`Imagem destacada da publicação ${title}`} />
-      <h1>{title}</h1>
-      <p>{content}</p>
+    <article className="article card">
+      <img className="article--img" src={img} alt={`Imagem destacada da publicação ${title}`} />
+      <h2 className="article--title">{title}</h2>
+      <p className="article--content">{content}</p>
+      <button className="article--button">Saiba mais</button>
     </article>
   )
 }
