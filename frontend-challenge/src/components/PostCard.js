5edbbf1547de5b3457f0e72c9881a0d187d9e7ad
Pedro Toselli <phtoselli@gmail.com>
2021-10-26 00:34:46 -0300

    Build: render posts elements using api

--- frontend-challenge/src/components/PostCard.js
+++ frontend-challenge/src/components/PostCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../style/PostCard.css';
 
-export default function Post({ img, title, content}) {
+export default function Post({ props: { title, _embedded } }) {
+
+  const img = _embedded['wp:featuredmedia'][0].source_url;
+
   return (
     <article className="article card">
       <img className="article--img" src={img} alt={`Imagem destacada da publicação ${title}`} />
-      <h2 className="article--title">{title}</h2>
-      <p className="article--content">{content}</p>
+      <h2 className="article--title">{title.rendered}</h2>
       <button className="article--button">Saiba mais</button>
     </article>
   )
--- frontend-challenge/src/components/Posts.js
+++ frontend-challenge/src/components/Posts.js
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style/Posts.css';
-import PostCard from './PostCard';
 import { v4 } from 'uuid';
 
-export default function Posts() {
-  const exampleResponse = [
-    {
-      img: 'imagem',
-      title: 'Titulo1',
-      content: 'Exemplo de conteudo de uma postagem'
-    },
-    {
-      img: 'imagem',
-      title: 'Titulo2',
-      content: 'Exemplo de conteudo de uma postagem'
-    },
-    {
-      img: 'imagem',
-      title: 'Titulo3',
-      content: 'Exemplo de conteudo de uma postagem'
-    }
-  ];
+import PostCard from './PostCard';
+import basicFetch from '../api/basicFetch';
 
-  const exampleElement = exampleResponse.map((curr) => {
-    return <PostCard key={v4()} img={curr.img} title={curr.title} content={curr.content} />
+const URL = 'https://blog.apiki.com/';
+const END_POINT = 'wp-json/wp/v2/posts?_embed&categories=518';
+
+export default function Posts() {
+  const [blogApi, setBlogApi] = useState([]);
+
+  const data = async () => await basicFetch(URL, END_POINT)
+    .then((response) => response)
+    .catch((err) => err);
+
+
+  useEffect(() => {
+    // com função iife
+    // (async function func() {
+    //   const resolve = await data();
+    // }())
+
+    data().then((res) => setBlogApi(res));
+  }, []);
+
+
+  const newCard = blogApi.map((curr) => {
+    return <PostCard key={v4()} props={ curr } />
   });
 
   return (
     <main className="main flex">
+      {console.log(blogApi)}
       <h1 id="main--title">Postagens</h1>
-      <div className="main--container flex">{ exampleElement }</div>
+      <div className="main--container flex">{ blogApi.length > 0 ? newCard : 'Carregando...' }</div>
     </main>
   )
 }
