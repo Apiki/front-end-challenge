ae147553b74ec213b7b0ed7d760bc269190ea3d8
Gui Pereira <guilhermepdo1508@gmail.com>
2021-11-12 20:15:25 -0300

    Feito e estilizado componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import facebook from '../images/facebook.png';
+import instagram from '../images/instagram.png';
+import linkedin from '../images/linkedin.png';
+import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <footer>
+    <a target="_blank" href="https://www.facebook.com/" rel="noreferrer">
+      <img src={facebook} alt="facebook" />
+    </a>
+    <a target="_blank" href="https://www.instagram.com/" rel="noreferrer">
+      <img src={instagram} alt="instagram" />
+    </a>
+    <a target="_blank" href="https://www.linkedin.com/" rel="noreferrer">
+      <img src={linkedin} alt="linkedin" />
+    </a>
+  </footer>
 );
 
 export default Footer;
