c13a601cc4669ef2500decbc69378dcc3007dbef
Rodrigo <rodrigopython16@gmail.com>
2021-11-15 19:33:02 -0300

    fix: fixing loading component with margin top

--- apiki-blog/src/components/Footer.jsx
+++ apiki-blog/src/components/Footer.jsx
@@ -11,9 +11,6 @@ function Footer () {
         <hr />
         <h1 className="footer__title">Apiki</h1>
         <p>Empresa especializada em WordPress com três unidades de negócios.</p>
-        <p>Design e Desenvolvimento;</p>
-        <p>Infra e Hospedagem;</p>
-        <p>Suporte e manutenção.</p>
         <p>Em resumo, só fazemos uma coisa em WordPress: tudo.</p>
         <p>2020 Apiki - Empresa especializada em WordPress.</p>
       </div>
