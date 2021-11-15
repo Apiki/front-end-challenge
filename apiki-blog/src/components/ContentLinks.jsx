ef8b247f8dcd3267c71196f9337fc23ec1e60830
Rodrigo <rodrigopython16@gmail.com>
2021-11-15 15:58:28 -0300

    fix: separeted social links and content links to another component

---
+++ apiki-blog/src/components/ContentLinks.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function ContentLinks () {
+  return (
+    <ul id="menu-content-links">
+      <li>
+        <a href="https://blog.apiki.com/web-stories/">Web Stories</a>
+      </li>
+      <li>
+        <a href="https://blog.apiki.com/infra/">Infra</a>
+      </li>
+      <li>
+        <a href="https://blog.apiki.com/mobile/">Mobile</a>
+      </li>
+      <li>
+        <a href="https://blog.apiki.com/performance/">Performance</a>
+      </li>
+      <li>
+        <a href="https://blog.apiki.com/seguranca/">Segurança</a>
+      </li>
+      <li>
+        <a href="https://blog.apiki.com/seo/">SEO</a>
+      </li>
+      <li>
+        <a href="https://blog.apiki.com/e-commerce/">E-commerce</a>
+      </li>
+    </ul>
+  )
+}
+
+export default ContentLinks
--- apiki-blog/src/components/Menu.jsx
+++ apiki-blog/src/components/Menu.jsx
@@ -1,77 +1,15 @@
 import React from 'react'
 
-import facebookIcon from '../imgs/icons/facebook.svg'
-import instagramIcon from '../imgs/icons/instagram.svg'
-import linkedinIcon from '../imgs/icons/linkedin.svg'
-import youtubeIcon from '../imgs/icons/youtube.svg'
+import ContentLinks from './ContentLinks'
+import SocialLinks from './SocialLinks'
 
 import '../css/Menu.css'
 
 function Menu () {
   return (
     <div id="menu">
-      <ul id="menu-content-links">
-        <li>
-          <a href="https://blog.apiki.com/web-stories/">Web Stories</a>
-        </li>
-        <li>
-          <a href="https://blog.apiki.com/infra/">Infra</a>
-        </li>
-        <li>
-          <a href="https://blog.apiki.com/mobile/">Mobile</a>
-        </li>
-        <li>
-          <a href="https://blog.apiki.com/performance/">Performance</a>
-        </li>
-        <li>
-          <a href="https://blog.apiki.com/seguranca/">Segurança</a>
-        </li>
-        <li>
-          <a href="https://blog.apiki.com/seo/">SEO</a>
-        </li>
-        <li>
-          <a href="https://blog.apiki.com/e-commerce/">E-commerce</a>
-        </li>
-      </ul>
-
-      <ul id="menu-social-links">
-        <li>
-          <a
-            href="https://www.facebook.com/ApikiWordPress"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={facebookIcon} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/apikiwordpress/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={instagramIcon} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/company/apiki/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={linkedinIcon} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.youtube.com/channel/UC__ToR3hqjs1ZktdLIWqYFA"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={youtubeIcon} />
-          </a>
-        </li>
-      </ul>
+      <ContentLinks />
+      <SocialLinks />
     </div>
   )
 }
