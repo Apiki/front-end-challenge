121535a0a49427ac2b3a3995cc0e4b2da04c42b5
Gui Pereira <guilhermepdo1508@gmail.com>
2021-11-16 12:08:40 -0300

    Feito deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import DataProvider from './context/DataProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DataProvider>
         <App />
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
