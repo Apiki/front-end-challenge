aca8890cc6cee2ae01d0bc81fba8acae9f236381
matheuscrok <zenorhan@hotmail.com>
2022-01-18 21:04:30 -0300

    data header

--- apiki/src/pages/posts/posts.jsx
+++ apiki/src/pages/posts/posts.jsx
@@ -4,18 +4,43 @@ import PostsList from '../../components/list_posts/postsList';
 import { Link } from 'react-router-dom';
 
 export default function Posts(){
+
   const [posts, setPosts] = useState(null);
+  
   useEffect(() => {
     const apiUrl = "https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518";
+    const headers = {  headers: {
+      "Content-Type": "application/json"
+ }, }
     const fetchItems = async () => {
-      const data = await fetch(`${apiUrl}`);
+      const data = await fetch(`${apiUrl}`,headers)
       const items = await data.json();
-
+      
       
       setPosts(items);
     }
     fetchItems();
   }, []);
+
+  function load(){
+    let page=1;
+    let urlPage=`https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518&page=${page}`
+    const fetchItems = async () => {
+      const data = await fetch(`${urlPage}`);
+      const items = await data.json();
+
+      console.log(data.headers.get('x-wp-totalpages'))
+      setPosts(items);
+    }
+    fetchItems();
+    page++;
+    return(
+      <Link to="" refresh="true"/>
+    )
+
+  }
+  
+
 
   return (
 
@@ -24,10 +49,9 @@ export default function Posts(){
 
         <PostsList posts={posts} />
         <div className='Footer' >
-      <Link to="/">
-              <button type="button">
+              <button type="button" onClick={()=>load()}>
                 Carregar Mais
-              </button></Link>
+              </button>
       </div>
     </div>
   
