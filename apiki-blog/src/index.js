996761c82e0aba7c75499c1982ef0a922beed923
GallegosEverson <gallegoseverson@gmail.com>
2022-01-23 15:33:23 -0300

    ReactDOM.hydrate

--- apiki-blog/src/index.js
+++ apiki-blog/src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.hydrate(
+    <App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
