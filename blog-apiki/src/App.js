24ff28089712bf5cc6dee78471243c7728fe9dcf
Pedro Toselli <phtoselli@gmail.com>
2022-01-25 18:56:33 -0300

    Build: criando Header da página

--- blog-apiki/src/App.js
+++ blog-apiki/src/App.js
@@ -1,6 +1,8 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
-    "Hello world"
+    <Home />
   );
 }
 
---
+++ blog-apiki/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <Header />
+  );
+}
