06a64a12e2d8eab6fb91331a0b7086960ce7b077
Pedro Toselli <phtoselli@gmail.com>
2022-01-26 15:29:34 -0300

    Build: implementando função de botão para carregar mais elementos

--- blog-apiki/src/pages/Home.jsx
+++ blog-apiki/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import '../style/Home.css'
 import api from '../services/api';
 import Card from '../components/Card';
 import Header from '../components/Header';
 
 export default function Home() {
-  const [news, setNews] = useState();
+  const [news, setNews] = useState([]);
+  const [response, setResponse] = useState(0);
+  const [page, setPage] = useState(2);
 
   useEffect(() => {
     api
@@ -15,18 +18,44 @@ export default function Home() {
       });
   }, []);
 
-  console.log(news)
+  useEffect(() => {
+    console.log('loop?')
+    if (news.length >= response['x-wp-total']) {
+      document.getElementById('carregar-mais-btn').disabled = true
+  }
+  }, [news, response]);
+
+  const CarregarMais = async () => {
+    const newsList = news;
+
+      await api
+        .get(`/posts?_embed&categories=518&page=${page}`)
+        .then((response) =>
+            setNews(newsList.concat(response.data),
+            setResponse(response.headers)
+          ))
+        .catch((err) => {
+          console.error("Ocorreu um erro:" + err);
+        });
+
+      setPage(page + 1)
+
+    }
 
   return (
     <>
       <Header />
-      <div>
+      <div className='home-news'>
         {
           news?
           news.map(curr => (
             <Card
               key={curr.id}
-              img={curr['_embedded']['wp:featuredmedia'][0].source_url}
+              img={
+                curr['_embedded']['wp:featuredmedia']
+                ? curr['_embedded']['wp:featuredmedia'][0]['source_url']
+                : ''
+            }
               slug={curr.slug}
               title={curr.title.rendered}
               link={curr.link}
@@ -35,6 +64,7 @@ export default function Home() {
           : 'Carregando...'
         }
       </div>
+      <button id='carregar-mais-btn' onClick={() => CarregarMais()}>Carregar mais...</button>
     </>
   );
 }
