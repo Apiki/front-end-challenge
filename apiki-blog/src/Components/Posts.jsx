ad111723be4e304c8804ff119e710b158e83d1a6
Angelo Bittencourt <angelogpbl.info@gmail.com>
2022-01-26 23:47:47 -0300

    adiciona botoes de mais posts

--- apiki-blog/src/Components/Posts.jsx
+++ apiki-blog/src/Components/Posts.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { getPosts } from "../Services/api";
+import { getPosts, getHeaders } from "../Services/api";
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function fetchApi() {
-      const postsFromApi = await getPosts();
+      const postsFromApi = await getPosts(page);
       setPosts(postsFromApi)
+      setHeaders(await getHeaders())
     }
     fetchApi()
-  }, [])
+  }, [page])
+
+  useEffect(() => {
+    console.log(posts)
+  }, [posts])
 
   return <div>
     {posts.map((post) => {
@@ -19,8 +26,16 @@ export default function Posts() {
           <a href={post.slug}>
           <h2> {post.title.rendered}</h2>
           </a>
-          <img src={post._embedded["wp:featuredmedia"][0].source_url} alt="img" />
-        </div>)
+          {post._embedded["wp:featuredmedia"] &&
+           <img src={post._embedded["wp:featuredmedia"][0].source_url} alt="img" width="350" />}
+    </div>)
     })}
+    <button 
+      type="button" 
+      onClick={ () => setPage(page + 1) }
+      disabled={ page === 17 ? true : false }
+    > 
+      Mais noticias 
+      </button>
   </div>;
 }
--- apiki-blog/src/Services/api.js
+++ apiki-blog/src/Services/api.js
@@ -1,7 +1,15 @@
 const POST_URL = "https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518"
 
-export async function getPosts() {
-  const request = await fetch(POST_URL);
+export async function getPosts(page) {
+  const request = await fetch(`${POST_URL}&page=${page}`);
   const response = await request.json();
   return response
 }
+
+export async function getHeaders() {
+  const request = await fetch(POST_URL);
+  const total = request.headers.get('X-WP-Total');
+  const totalPages = request.headers.get('X-WP-TotalPages')
+  return [total, totalPages]
+}
+
