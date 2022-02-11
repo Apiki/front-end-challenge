39321f7f4cc0c46cc861b6918481e3b406382f2c
Andre Parada <andre.parada@id-cotton.com>
2022-02-11 14:15:36 -0400

    Background

--- src/app/pages/initial/initial.component.ts
+++ src/app/pages/initial/initial.component.ts
@@ -12,61 +12,79 @@ import { InitialService } from './initial.service';
 })
 export class InitialComponent implements OnInit {
 
-page: number = 1;
-notices: any;
-aut: any;
-slug: string;
-image: string;
+  page: number = 1;
+  notices: any;
+  aut: any;
+  slug: string;
+  image: string;
+  varcount: number = 0;
 
-constructor(
-  private initialService: InitialService,
-  private route: Router,
-  private internalService: InternalPostService
+  constructor(
+    private initialService: InitialService,
+    private route: Router,
+    private internalService: InternalPostService
   ) { }
-ngOnInit(): void {
+  ngOnInit(): void {
 
-this.getNotices();
+    this.getNotices();
 
-}
+  }
 
-getNotices() {
+  getNotices() {
 
-  this.initialService.getNotice(this.page).subscribe((notices: any) => {
-    if (this.notices == undefined) {
-      this.notices = notices;
-      for(let Autores of notices){
-        if(Autores._embedded.author[0].name == undefined)
-        {
-          Autores._embedded.author[0].name = 'Autor Desconhecido'
+    this.initialService.getNotice(this.page).subscribe((notices: any) => {
+      if (this.notices == undefined) {
+        this.notices = notices;
+        for (let Autores of notices) {
+          if (Autores._embedded.author[0].name == undefined) {
+            Autores._embedded.author[0].name = 'Autor Desconhecido'
 
+          }
         }
+        console.log(this.notices);
+
+      } else {
+        this.notices = this.notices.concat(notices)
+        console.log(this.notices);
       }
-      console.log(this.notices);
-      
-    } else {
-      this.notices = this.notices.concat(notices)
-      console.log(this.notices);
-    }
-    
-    for(let image of notices){
-      image = image._embedded.author[0].avatar_urls[48]
-      this.image = image
-     
-      
-      console.log(this.image)
-    }
-  });
-}
+      var i = 0
+      for (let image of notices) {
+        //console.log(`${this.varcount++}`, image._embedded.author[0].avatar_urls[48])
+        try {
+          image._embedded.author[0].imgAuthor = image._embedded.author[0]?.avatar_urls[48]
+        } catch (erro: any) {
+          image._embedded.author[0].imgAuthor = 'https://i.pravatar.cc/40?img=1'
+          console.log(image._embedded.author[0].imgAuthor)
+        }
 
-handleReadMore(link:string){
-  this.internalService.slug = link
-  this.route.navigate(['/internal-post'])
-}
 
-handleLoadMore(){
-  this.page++;
-  this.initialService.getNotice(this.page)
-  this.getNotices();
-}
+        /*if(image._embedded.author[0].data.status != 'undefined'){
+          console.log('i: ', i)
+          i++
+          console.log('1: ',image._embedded.author[0].avatar_urls[48] )
+          image._embedded.author[0].imgAuthor = image._embedded.author[0].avatar_urls[48]
+        }else{
+          image = null
+          image._embedded.author[0].avatar_urls[48] = null
+          console.log('entrou:',this.image)
+        }*/
+
+
+
+        //console.log(this.image)
+      }
+    });
+  }
+
+  handleReadMore(link: string) {
+    this.internalService.slug = link
+    this.route.navigate(['/internal-post'])
+  }
+
+  handleLoadMore() {
+    this.page++;
+    this.initialService.getNotice(this.page)
+    this.getNotices();
+  }
 
 }
