0e0e21451cd857f7719578eadbcf2f8da49f1c1a
unknown <marcus.urani20@gmail.com>
2022-06-09 17:38:39 -0300

    Finalizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import Page404 from "./pages/Page404";
 import InternPage from "./pages/InternPage";
@@ -15,7 +14,6 @@ function App() {
         <Route path="/:slug" element={<InternPage />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
