5ea0ea88a7e20a9b2600b55daf660e81df45a2af
Fernando Gurgel <gurgelff@gmail.com>
2022-08-04 19:04:48 -0300

    feat: add support for sass

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,10 @@ type Data = {
   name: string
 }
 
+type Res = NextApiResponse<Data>
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: Res
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
