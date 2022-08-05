8493d11074a47110152308ebf2dcb1f210639bd4
Fernando Gurgel <gurgelff@gmail.com>
2022-08-04 23:53:51 -0300

    feat: basic posts list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,52 @@
-import type { NextPage } from 'next'
-import axios from 'axios'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import type { NextPage } from "next";
+import axios from "axios";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import Posts, { PostsProps } from "../types/Posts";
 
-type Posts = Post[]
-type Post = {
-
-  slug: string,
-  link: string,
-  yoast_head: string,
-  title: {
-    rendered: string
-  },
-  excerpt: {
-    rendered: string
-  },
-  _embedded: {
-    'wp:featuredmedia': [
-      {
-        source_url: string,
-        media_type: string,
-        media_details: {
-          sizes: {
-            medium: {
-              source_url: string
-            }
-          }
-        }
-      }
-    ]
-  }
-
-}
-
-type PostsProps = {
-  title: string,
-}
-
-const Home: NextPage<PostsProps> = ({title}) => {
+const Home: NextPage<PostsProps> = ({ posts }) => {
   return (
-    <h1>{title}</h1>
-  )
-}
+    <main>
+      {posts.map((post) => {
+        const ratio = post.width / post.height ?? 1;
+        return (
+          <article key={post.slug}>
+            <h2>{post.title}</h2>
+            <Image width={350} height={350 / ratio} src={post.image} />
+            <span dangerouslySetInnerHTML={{ __html: post.excerpt }}></span>
+            <a href={post.link}>{post.slug}</a>
+          </article>
+        );
+      })}
+    </main>
+  );
+};
 
 export const getStaticProps = async () => {
   const tenHours = 10 * 60 * 60;
-  const lastTenPostsUrl = 'https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518';
-  const posts: Posts = await (await axios.get(lastTenPostsUrl)).data;
-  
+  const lastTenPostsUrl =
+    "https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518";
+  const postsData: Posts = await (await axios.get(lastTenPostsUrl)).data; // @TODO: destruc as
+
+  const posts = postsData.map((post) => {
+    return {
+      title: post.title.rendered,
+      link: post.link,
+      slug: post.slug,
+      image: post._embedded["wp:featuredmedia"][0].source_url,
+      width: post._embedded["wp:featuredmedia"][0].media_details.width,
+      height: post._embedded["wp:featuredmedia"][0].media_details.height,
+      excerpt: post.excerpt.rendered,
+    };
+  });
+
   return {
     props: {
-      title: posts[0].title.rendered,
+      posts,
     },
-    revalidate: tenHours
-  }
+    revalidate: tenHours, // 10 hours is the industry standard that complies with certain scripts
+  };
+};
 
-}
-
-export default Home
+export default Home;
