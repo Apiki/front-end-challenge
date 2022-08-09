45d443003aa5d33a07761c650795f990de034c1f
Fernando Gurgel <gurgelff@gmail.com>
2022-08-09 02:37:02 -0300

    feat: Page Component to proper track pagination

---
+++ pages/page/[slug].tsx
@@ -0,0 +1,165 @@
+import type { GetServerSideProps, NextPage } from "next";
+import { useRouter } from "next/router";
+import { decode } from "html-entities";
+import { sanitize } from "isomorphic-dompurify";
+import Head from "next/head";
+import Image from "next/image";
+import { PostsProps, PostState } from "../../types/Posts";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { fetchPosts } from "../../utils/post";
+import styles from "../../styles/Home.module.scss";
+import AuthorSection from "../../components/AuthorSection";
+
+const getTenPostsUrl =
+  "https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518";
+
+const Page: NextPage<PostsProps> = ({ posts, totalPages, page }) => {
+  console.log("Page.page ", page);
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(page);
+  const [pagePosts, setPagePosts] = useState<PostState[]>(posts);
+
+  useEffect(() => {
+    if (currentPage > totalPages) {
+      setCurrentPage(totalPages);
+      return;
+    }
+    if (currentPage < 1) {
+      setCurrentPage(1);
+      return;
+    }
+
+    // don't make useEffect async, make another func for it
+    const getPosts = async () => {
+      const postsOptions = { params: { page: currentPage } };
+      const { posts } = await fetchPosts(getTenPostsUrl, postsOptions);
+      setPagePosts(posts);
+    };
+
+    getPosts();
+  }, [currentPage]);
+
+  return (
+    <>
+      <Head>
+        <title>Blog da Apiki | Desenvolvimento </title>
+        <meta name="description" content="Blog da Apiki" />
+        <meta
+          name="keywords"
+          content="blog, apiki, desenvolvimento, wordpress"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="language" content="pt-br" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <h1 className={styles.blog_title}>Blog da Apiki</h1>
+      <main
+        className={styles.posts_container}
+        role="main"
+        aria-label="Conteúdo Principal"
+      >
+        {pagePosts.map((post) => {
+          const ratio = (post.imageWidth ?? 384) / (post.imageHeight ?? 202); // defaults to 1.9
+          const post_date = new Date(post.date);
+          const date_options: Intl.DateTimeFormatOptions = {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          };
+          return (
+            <article
+              role="card"
+              aria-label="Post Card"
+              className={styles.post_card}
+              key={post.slug}
+            >
+              <h2 className={styles.post_title}>{decode(post.title)}</h2>
+              {post.imageURL ? (
+                <Image
+                  width={350}
+                  height={350 / ratio}
+                  src={post.imageURL}
+                  alt={post.alt}
+                />
+              ) : null}
+              <span
+                className={styles.post_description}
+                dangerouslySetInnerHTML={{ __html: sanitize(post.excerpt) }}
+              />
+              <div className={styles.post_card_footer}>
+                <AuthorSection
+                  hideOnMobile={true}
+                  avatar={post.avatar}
+                  authorLink={post.authorLink}
+                  authorName={post.authorName}
+                  date={post.date}
+                />
+                <Link href={`/post/${post.slug}`}>
+                  <a
+                    role="button"
+                    aria-label="Botão para ler mais detalhes sobre o post"
+                    className={styles.post_button}
+                  >
+                    <strong>Ler mais</strong>
+                  </a>
+                </Link>
+              </div>
+            </article>
+          );
+        })}
+      </main>
+      <div className={styles.pagination}>
+        {totalPages > 1 ? (
+          <>
+            <button
+              className={styles.pagination_button}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentPage(currentPage - 1);
+                router.push(`/page/${currentPage - 1}`, undefined, {
+                  scroll: false,
+                });
+              }}
+              disabled={currentPage === 1}
+            >
+              Anterior
+            </button>
+            <span className={styles.pagination_counter}>
+              {currentPage} de {totalPages}
+            </span>
+            <button
+              className={styles.pagination_button}
+              onClick={(e) => {
+                e.preventDefault();
+                setCurrentPage(currentPage + 1);
+                router.push(`/page/${currentPage + 1}`, undefined, {
+                  scroll: false,
+                });
+              }}
+              disabled={currentPage === totalPages}
+            >
+              Próxima
+            </button>
+          </>
+        ) : null}
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const postsOptions = { params: { page: context.query.slug } };
+  const { posts, headers } = await fetchPosts(getTenPostsUrl, postsOptions);
+
+  return {
+    props: {
+      posts,
+      totalPages: parseInt(headers["x-wp-totalpages"] as string) ?? 1,
+      page: parseInt(context.query.slug as string) ?? 1,
+    },
+  };
+};
+
+export default Page;
