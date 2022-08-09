9d0bf82f9c125eb74a3bfb008be215dd2190f44a
Fernando Gurgel <gurgelff@gmail.com>
2022-08-09 02:44:33 -0300

    feat: Author type-related features and type for Post Static Props

--- types/Posts.ts
+++ types/Posts.ts
@@ -1,10 +1,12 @@
 import { AxiosRequestHeaders } from "axios";
+import {ParsedUrlQuery} from "querystring";
 
 type Posts = Post[];
 
 export type Post = {
   slug: string;
   link: string;
+  date: string;
   yoast_head: string;
   title: {
     rendered: string;
@@ -12,7 +14,22 @@ export type Post = {
   excerpt: {
     rendered: string;
   };
+  content: {
+    rendered: string;
+  };
   _embedded: {
+    author: [
+      {
+        name: string;
+        description: string;
+        link: string;
+        avatar_urls: {
+          24: string;
+          48: string;
+          96: string;
+        };
+      }
+    ];
     "wp:featuredmedia": [
       {
         source_url: string;
@@ -30,22 +47,52 @@ export type Post = {
 export type PostsProps = {
   posts: PostState[];
   totalPages: number;
+  page: number;
 };
 
 export type PostState = {
-      title: string;
-      link: string;
-      slug: string;
-      imageURL: string;
-      imageWidth: number;
-      imageHeight: number;
-      alt: string;
-      excerpt: string;
+  title: string;
+  link: string;
+  slug: string;
+  authorName: string;
+  authorDescription: string;
+  authorLink: string;
+  avatar: string;
+  date: string;
+  imageURL: string;
+  imageWidth: number;
+  imageHeight: number;
+  alt: string;
+  excerpt: string;
+  content: string;
+  yoast_head: string;
 };
 
 export type PostsResponse = {
   data: Posts;
   headers: AxiosRequestHeaders;
 };
+
+export type SinglePostProps = {
+  title: string;
+  link: string;
+  slug: string;
+  content: string;
+  date: string;
+  authorName: string;
+  authorDescription: string;
+  avatar: string;
+  authorLink: string;
+  imageURL: string;
+  imageWidth: number;
+  imageHeight: number;
+  alt: string;
+  excerpt: string;
+  yoast_head: string;
+};
+
+export interface SinglePostParams extends ParsedUrlQuery {
+  slug: string;
+}
 
 export default Posts;
