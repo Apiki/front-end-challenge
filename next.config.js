5d922c5b347c7308d00b5979682aa482d0570883
Fernando Gurgel <gurgelff@gmail.com>
2022-08-09 02:41:13 -0300

    feat: add gravatar to image domains

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['blog.apiki.com'],
+    domains: ["blog.apiki.com", "secure.gravatar.com"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
