ff831b2e5eb33d5985096c5a8425ed670646ae6c
Fernando Gurgel <gurgelff@gmail.com>
2022-08-09 04:13:32 -0300

    impr: [utils/post] complete props checking

--- utils/post.ts
+++ utils/post.ts
@@ -9,23 +9,23 @@ export const fetchPosts = async (url: string, options: AxiosRequestConfig) => {
 
   const posts = postsData.map((post) => {
     return {
-      title: post.title.rendered,
-      link: post.link,
-      slug: post.slug,
-      content: post.content.rendered,
-      date: post.date,
-      authorName: post._embedded.author?.[0]?.name ?? "",
-      authorDescription: post._embedded.author?.[0]?.description ?? "",
-      avatar: post._embedded.author?.[0]?.avatar_urls?.["96"] ?? "",
-      authorLink: post._embedded.author?.[0]?.link ?? "",
-      imageURL: post._embedded["wp:featuredmedia"]?.[0]?.source_url ?? "",
+      title: post?.title?.rendered ?? "",
+      link: post?.link ?? "",
+      slug: post?.slug ?? "",
+      content: post?.content?.rendered ?? "",
+      date: post?.date ?? "",
+      authorName: post?._embedded?.author?.[0]?.name ?? "",
+      authorDescription: post?._embedded?.author?.[0]?.description ?? "",
+      avatar: post?._embedded?.author?.[0]?.avatar_urls?.["96"] ?? "",
+      authorLink: post?._embedded?.author?.[0]?.link ?? "",
+      imageURL: post?._embedded?.["wp:featuredmedia"]?.[0]?.source_url ?? "",
       imageWidth:
-        post._embedded["wp:featuredmedia"]?.[0]?.media_details?.width ?? null,
+        post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.width ?? null,
       imageHeight:
-        post._embedded["wp:featuredmedia"]?.[0]?.media_details?.height ?? null,
-      alt: post._embedded["wp:featuredmedia"]?.[0]?.alt_text ?? "",
-      excerpt: post.excerpt.rendered,
-      yoast_head: post.yoast_head,
+        post?._embedded?.["wp:featuredmedia"]?.[0]?.media_details?.height ?? null,
+      alt: post?._embedded?.["wp:featuredmedia"]?.[0]?.alt_text ?? "",
+      excerpt: post?.excerpt?.rendered ?? "",
+      yoast_head: post?.yoast_head ?? "",
     };
   });
 
