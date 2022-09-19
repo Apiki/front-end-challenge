cdebbf67370badde31f2b29306ee66d513f6d3c1
felipetexa <felipe.teixeira91@hotmail.com>
2022-09-19 16:41:10 -0300

    refatorando, aplicando lógica do load more, axios

--- src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.css';
-
-export default function Button(props) {
-  return (
-    <div className={styles.button}>
-      <button type='button'>{props.title}</button>
-    </div>
-  )
-}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,71 @@
 import Header from '../components/Header/index.jsx';
 import Cards from '../components/Cards/index.jsx';
-import Button from '../components/Button/index.jsx';
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import styles from '../styles/Button.module.css'
 
 export default function Home() {
+
+  const [loading, setLoading] = useState(false);
+  const [cardData, setCardData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(0);
+
+  const fetchAllData = async () => {
+      setLoading(true);
+      const response = await axios.get(`https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518&page=${page}`)
+      setTimeout(() => {
+        setCardData(response.data)
+      }, 1000);
+      setMaxPage(response.headers['x-wp-totalpages'])
+      setLoading(false);
+    }
+
+  const loadMore = async () => {
+    setPage(page+1);
+
+    if (page == maxPage - 1) {
+      return;
+    } else {
+      setLoading(true);
+      const { data } = await axios.get(
+        `https://blog.apiki.com/wp-json/wp/v2/posts?_embed&categories=518&page=${page}`
+      );
+      setCardData(cardData.concat(data));
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllData();
+  }, [])
 
   return (
     <div>
       <Header />
-      <Cards />
-      
-      <Button title={'Carregar mais...'} />
+      {loading && !cardData && 
+      <p>Carregando informações</p>
+        }
+      {cardData && cardData.map((item) => (
+      <Cards 
+      key={item.id}
+      imgUrl={item._embedded['wp:featuredmedia'] &&
+            item._embedded['wp:featuredmedia'][0].source_url
+            }
+      title={item.title.rendered}
+      slug={item.slug}
+      description={item.yoast_head_json.description}/>
+      ))}
+      {page == maxPage ? (
+              ""
+            ) : (
+      <div className={styles.button}>
+        <button onClick={() => loadMore()}>Carregar Mais...</button>
+      </div>
+      )}
+
     </div>
   )
 }
