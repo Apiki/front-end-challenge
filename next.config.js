4e3d437bff5560de6cfb1c7c0b9efc7f4fa52b4e
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2023-02-14 08:10:12 -0300

    chore: install Styled-Component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
